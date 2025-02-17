9d0832b4001bf7e238b83b4dfadfd43d2601361e
hbittar <hassankarimbittar@gmail.com>
2025-02-17 13:56:47 -0300

    added admin for user code

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,15 +4,13 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 from users.forms import UserChangeForm, UserCreationForm
+from users.models import UserVerificationCode
 
 User = get_user_model()
 
 
-# @admin.register(User)
-# class UserAdmin(auth_admin.UserAdmin):
-#
-#     form = UserChangeForm
-#     add_form = UserCreationForm
-#     list_display = ["username", "name", "is_superuser", 'is_active']
-#     search_fields = ["name"]
-#
+class UserVerificationCodeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserVerificationCode, UserVerificationCodeAdmin)
