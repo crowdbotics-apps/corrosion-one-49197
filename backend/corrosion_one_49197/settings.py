327bc393c8ac28d93bdf8cb5596dc62915f15cd3
Tolu Ayantunde <ayantundetolu@gmail.com>
2025-02-19 19:38:57 +0100

    Update settings.py

--- backend/corrosion_one_49197/settings.py
+++ backend/corrosion_one_49197/settings.py
@@ -206,6 +206,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'web_build/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+# Configuration for Azure Storage
+AS_BUCKET_NAME = env.str("AS_BUCKET_NAME", "")
+if AS_BUCKET_NAME:
+    AZURE_ACCOUNT_NAME = AS_BUCKET_NAME
+    AZURE_TOKEN_CREDENTIAL = DefaultAzureCredential()
+    AS_STATIC_CONTAINER = env.str("AS_STATIC_CONTAINER", "static")
+    AS_MEDIA_CONTAINER = env.str("AS_MEDIA_CONTAINER", "media")
+    AZURE_URL_EXPIRATION_SECS  = env.int("AZURE_URL_EXPIRATION_SECS", 3600)
+    DEFAULT_FILE_STORAGE = "corrosion_one_49197.storage_backends.AzureMediaStorage"
+    STATICFILES_STORAGE = "corrosion_one_49197.storage_backends.AzureStaticStorage"
 
 
 # MEDIA_URL = '/mediafiles/'
