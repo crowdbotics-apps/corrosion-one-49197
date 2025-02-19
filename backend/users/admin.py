dfe3b8bf40be77aff351d5ef9676775d7b87bfee
hbittar <hassankarimbittar@gmail.com>
2025-02-19 16:48:04 -0300

    updated settings.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -18,4 +18,22 @@ admin.site.register(UserVerificationCode, UserVerificationCodeAdmin)
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
-    pass
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = (("User", {"fields": ("phone_number", "phone_verified")}),) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "phone_number", "is_superuser","is_staff", "phone_verified", "profile_picture"]
+    search_fields = ["username", "phone_number"]
+    list_filter = ["is_superuser"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(Q(is_superuser=False) & Q(is_staff=False))
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields["is_superuser"].disabled = True
+            form.base_fields["is_staff"].disabled = True
+        return form
