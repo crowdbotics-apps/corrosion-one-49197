e0fb5cfde6d0ffa931ab2a3716cb743274c45eef
hbittar <hassankarimbittar@gmail.com>
2025-02-22 22:34:48 -0300

    added sentry to the frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import {HashRouter} from "react-router-dom";
+import * as Sentry from "@sentry/react";
 import App from "App";
 
 // Material Dashboard 3 PRO React Context Provider
@@ -24,6 +25,21 @@ import StoreApp from "./StoreApp";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+
+}
 
 root.render(
   <HashRouter>
