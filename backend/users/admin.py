caa940706a48f7a6986c3298034c1d88892ede89
hbittar <hassankarimbittar@gmail.com>
2025-03-28 15:43:23 -0300

    Refactor UserAdmin registration: simplify registration by using auth_admin.UserAdmin directly

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -16,24 +16,28 @@ class UserVerificationCodeAdmin(admin.ModelAdmin):
 admin.site.register(UserVerificationCode, UserVerificationCodeAdmin)
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("phone_number", "phone_verified")}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "phone_number", "is_superuser","is_staff", "phone_verified", "profile_picture"]
-    search_fields = ["username", "phone_number"]
-    list_filter = ["is_superuser"]
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     form = UserChangeForm
+#     add_form = UserCreationForm
+#     fieldsets = (("User", {"fields": ("phone_number", "phone_verified")}),) + auth_admin.UserAdmin.fieldsets
+#     list_display = ["username", "phone_number", "is_superuser","is_staff", "phone_verified", "profile_picture"]
+#     search_fields = ["username", "phone_number"]
+#     list_filter = ["is_superuser"]
+#
+#     def get_queryset(self, request):
+#         qs = super().get_queryset(request)
+#         if request.user.is_superuser:
+#             return qs
+#         return qs.filter(Q(is_superuser=False) & Q(is_staff=False))
+#
+#     def get_form(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj, **kwargs)
+#         if not request.user.is_superuser:
+#             form.base_fields["is_superuser"].disabled = True
+#             form.base_fields["is_staff"].disabled = True
+#         return form
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(Q(is_superuser=False) & Q(is_staff=False))
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if not request.user.is_superuser:
-            form.base_fields["is_superuser"].disabled = True
-            form.base_fields["is_staff"].disabled = True
-        return form
+
+admin.site.register(User, auth_admin.UserAdmin)
