b3749f0a22b4f111d910cd2cb5fa48a3f8bb7b3a
hbittar <hassankarimbittar@gmail.com>
2025-04-01 15:21:25 -0300

    Enhance job management: change verification code input type from number to text

--- frontend/src/pages/common/support/index.js
+++ frontend/src/pages/common/support/index.js
@@ -1,25 +1,38 @@
-
 import AdminLayout from "../../../components/AdminLayout";
-import React, { useEffect, useState } from "react";
-import { FormikProvider, useFormik } from "formik"
+import React, {useEffect, useState} from "react";
+import {FormikProvider, useFormik} from "formik"
 import Grid from "@mui/material/Grid"
 import Card from "@mui/material/Card"
 import FormikInput from "../../../components/Formik/FormikInput"
 import MDBox from "../../../components/MDBox"
 import MDButton from "../../../components/MDButton"
 import * as Yup from "yup"
+import {useApi} from "../../../services/helpers";
 
 function Support() {
+  const api = useApi()
   const [loading, setLoading] = useState(false);
+
+  const support = (values) => {
+    setLoading(true)
+    api.support(values).handle({
+      successMessage: 'Support request sent successfully',
+      onSuccess: () => {
+        formik.resetForm()
+      },
+      errorMessage: 'Error sending support request',
+      onFinally: () => setLoading(false)
+    })
+  }
 
 
   const initialValues = {
-    Subject: '',
+    subject: '',
     description: '',
   };
 
   const validationSchema = Yup.object().shape({
-    Subject: Yup.string().required('Subject is required'),
+    subject: Yup.string().required('Subject is required'),
     description: Yup.string().required('Description is required'),
   });
 
@@ -29,43 +42,31 @@ function Support() {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: (values) => {
-      const dataToSend = {
-        ...values,
-      }
+      support(values)
     },
   });
-
-
 
 
   return (
     <AdminLayout title={'Support'} showCard>
       <FormikProvider value={formik}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} lg={12}>
-            <Card sx={{p: 3, height: '100%'}}>
-              <FormikInput name="Subject" label="Subject" type="text" errors={formik.errors} mb={2}/>
-              <FormikInput
-                type="textarea"
-                label="Description"
-                name="description"
-                rows={5}
-                mb={2}
-              />
+        <FormikInput name="subject" label="Subject" type="text" errors={formik.errors} mb={2}/>
+        <FormikInput
+          type="textarea"
+          label="Description"
+          name="description"
+          rows={5}
+          mb={2}
+        />
 
-              <MDBox display="flex" justifyContent="flex-end" mr={1} mt={'auto'}>
-                <MDButton
-                  color={'secondary'}
-                  onClick={formik.handleSubmit}
-                  loading={loading}
-                  disabled={loading}
-                >Submit</MDButton>
-              </MDBox>
-
-            </Card>
-          </Grid>
-
-        </Grid>
+        <MDBox display="flex" justifyContent="flex-end" mr={1} mt={'auto'}>
+          <MDButton
+            color={'secondary'}
+            onClick={formik.handleSubmit}
+            loading={loading}
+            disabled={loading}
+          >Submit</MDButton>
+        </MDBox>
       </FormikProvider>
     </AdminLayout>
   );
